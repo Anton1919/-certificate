502ec9642efcfbe33973ea441102d8103fcef320
Anton1919 <bf3kenzo@mail.ru>
2023-03-12 21:50:13 +0300

    add cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.css'
+import group from './img/group.jpg'
+import lingua from './img/lingua.jpg'
+import certifivate from './img/certifivate.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={s.App}>
+			<div className={s.wrapper}>
+				<div className={s.card}>
+					<div className={s.label}><span>Фото будут другие, а это заглушки 😄</span></div>
+					<img src={certifivate} alt="photo"/>
+				</div>
+				<div className={s.card}>
+					<div className={s.label}><span>Фото будут другие, а это заглушки 😄</span></div>
+					<img src={group} alt="photo"/>
+				</div>
+				<div className={s.card}>
+					<div className={s.label}><span>Фото будут другие, а это заглушки 😄</span></div>
+					<img src={lingua} alt="photo"/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
